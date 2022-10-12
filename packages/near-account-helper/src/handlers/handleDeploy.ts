29fedb7dbc4358a53007a9f7ccdb41c8417b6ba3
daoauth <57783762+daoauth@users.noreply.github.com>
2022-10-12 16:08:33 +0900

    fix: deploy

--- packages/near-account-helper/src/handlers/handleDeploy.ts
+++ packages/near-account-helper/src/handlers/handleDeploy.ts
@@ -4,4 +4,5 @@ import { Deploy } from "../../generated/schema";
 export function handleDeploy(action: near.DeployContractAction, id: string, to: string): void {
     const deployData = new Deploy(id);
     deployData.codeHash = action.codeHash.toHexString();
+    deployData.save();
 }
